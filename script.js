9b9eb1a5265759f5773b8d33c23b70ab6c48fd16
Guilherme Mattioli <djmattioli@gmail.com>
2018-02-05 02:59:04 -0200

    BSOD

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
-// making the canvas full screen
-c.height = window.innerHeight;
-c.width = window.innerWidth;
-
 // the characters
 var gurmukhi = "੧੨੩੪੫੬੭੮੯੦ੳਅਰਤਯਪਸਦਗਹਜਕਲਙੜਚਵਬਨਮੲਥਫਸ਼ਧਘਝਖਲ਼ੜ੍ਹਛਭਣ"
 var sanskrit = "१२३४५६७८९अरतयपसदगहजकलङषचवबनमआथय़फशधघझखळक्षछभणऒ"
 // converting the string into an array of single characters
 var characters = sanskrit.split("");
 var font_size = 16;
-var columns = c.width/font_size;    // number of columns for the rain
+var columns = 1;    // number of columns for the rain
 
 // an array of drops - one per column
 var drops = [];
-// x below is the x coordinate
-// 1 = y-coordinate of the drop (same for every drop initially)
-for (var x = 0; x < columns; x++) drops[x] = 1;
 
+function ajusta_ctx () {
+	// making the canvas full screen
+	c.height = window.innerHeight;
+	c.width = window.innerWidth;
+	columns = c.width/font_size;    // number of columns for the rain
 
+	drops = [];
+	// x below is the x coordinate
+	// 1 = y-coordinate of the drop (same for every drop initially)
+	for (var x = 0; x < columns; x++) drops[x] = 1;
+}
 
 // drawing the characters
 function draw() {
@@ -64,28 +67,47 @@ press_start = function () {
 press_start_title = function () {
 	ctx.fillStyle = "rgba(0,0,0,1)";
 	ctx.fillRect(0, 0, c.width, c.height);
-	ctx.fillRect(0, 0, c.width, c.height);
 
 	ctx.fillStyle = "#FFF"; // text color
 	ctx.font = font_size + "px arial";
 
 	var text = "Press Start";
-	ctx.fillText(text, c.width/2.0 - ((text.length / 2.0) * font_size), c.height/2.0 - (font_size / 2.0));
+	ctx.fillText(
+		text,
+		c.width/2.0 - ((text.length * font_size) / 5.0 /*numero magico para centralizar*/),
+		c.height/2.0 - (font_size / 2.0)
+	);
 
 	start_timeout = setTimeout(press_start, interval_start);
 }
 
-function start_pressed(event) {
-	if (event.keyCode == 13) {
-		clearTimeout(start_timeout);
-		ctx.fillStyle = "rgba(0,0,0,1)";
-		ctx.fillRect(0, 0, c.width, c.height);
-		ctx.fillRect(0, 0, c.width, c.height);
+function bsod() {
+	ctx.fillStyle = "rgba(0,0,255,1)";
+	ctx.fillRect(0, 0, c.width, c.height);
 
-		setInterval(draw, 33);
-	}
+	$("#bsod_screen").show();
+
+	setTimeout(function () {
+		count_start = 1;
+	}, 1000)
 }
 
+var count_start = 0;
+function start_pressed(event) {
+	if (event.keyCode == 13 && !count_start) {
+		clearTimeout(start_timeout);
+		bsod();
+		count_start = -1;
+	} else if (count_start === 1) {
+		$("#bsod_screen").hide();
+
+		ctx.fillStyle = "rgba(0,0,0,1)"; //getColor();
+		ctx.fillRect(0, 0, c.width, c.height);
+
+		setInterval(draw, 50);
+		count_start = 2;
+	}
+}
 
 $(document).ready(function () {
 	ctx.fillStyle = "rgba(0,0,0,1)"; //getColor();
@@ -93,4 +115,7 @@ $(document).ready(function () {
 
 	press_start();
 	$("body").keydown(start_pressed);
+
+	ajusta_ctx();
+	// $(window).resize(ajusta_ctx);
 });
